252db352f877fd0c4f39afe22861fc2cda64377e
sunilkarki <karkisunil1200@gmail.com>
2021-09-20 13:49:57 -0500

    nepali

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yo maan ta mero Nepali ho. Sadhai ko lagi kancha nani </p>
+        <p>Yo maan ta mero Nepali ho</p>
         <a
           className="App-link"
           href="https://reactjs.org"
